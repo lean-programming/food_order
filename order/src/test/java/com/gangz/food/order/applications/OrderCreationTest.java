5d974ae011706edfb4d6b47bc38c95f9fd912039
Gang ZHANG <gangz2009@gmail.com>
2021-05-25 23:29:08 +0800

    rename test files

--- order/src/test/java/com/gangz/food/order/applications/OrderCreationTest.java
+++ order/src/test/java/com/gangz/food/order/applications/OrderCreationTest.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestOrderCreation {
+public class OrderCreationTest {
     @Test
     public void createOrderShouldBeSuccess(){
         String userId = UUID.randomUUID().toString();
--- order/src/test/java/com/gangz/food/order/applications/OrderFactoryTest.java
+++ order/src/test/java/com/gangz/food/order/applications/OrderFactoryTest.java
@@ -1,10 +1,12 @@
 package com.gangz.food.order.applications;
 
+
 import com.gangz.food.order.domain.order.Order;
 import com.gangz.food.order.domain.order.OrderFactory;
 import com.gangz.food.order.domain.order.OrderStatus;
 import com.gangz.food.order.infrastructure.food.FoodId;
 import com.gangz.food.order.infrastructure.user.UserId;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,25 +14,22 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-public class TestOrderApplication {
-    OrderApplicaton orderApplicaton;
+public class OrderFactoryTest {
+    OrderFactory orderFactory;
     @BeforeEach
     public void setUp(){
-        orderApplicaton = new OrderApplicaton(null);
+        orderFactory = new OrderFactory();
     }
     @Test
     public void createOrderShouldBeSuccess(){
         List<OrderItemDTO> selectedItems =
-                Arrays.asList(OrderItemDTO.build(FoodId.of("food_1"),1),
-                        OrderItemDTO.build(FoodId.of("food_2"),1));
-
-        OrderDTO order = orderApplicaton.create(UserId.of("user_1"),
-                selectedItems);
-        assertEquals(OrderStatus.SUBMITTED.name(),order.getStatus());
+        Arrays.asList(OrderItemDTO.build(FoodId.of("food_1"),1),
+                      OrderItemDTO.build(FoodId.of("food_2"),1));
+                      
+        Order order = orderFactory.create(UserId.of("user_1"),
+                                          selectedItems);
+        assertEquals(OrderStatus.SUBMITTED,order.status());
         assertEquals(2,order.itemCount());
     }
-
-
 }
